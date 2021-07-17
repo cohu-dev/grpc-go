9690640915b9193625098f9f0f587fa115d941b5
cota <65804288+cota-eng@users.noreply.github.com>
2021-07-17 13:34:48 +0900

    implemented server streaming

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/cota-eng/grpc-go/calculator/calculator_pb"
@@ -16,17 +17,39 @@ func main() {
 	}
 	defer conn.Close()
 	c := calculator_pb.NewCalculatorServiceClient(conn)
-	doUnary(c)
+	// doUnary(c)
+	doServerStreaming(c)
 }
 
-func doUnary(c calculator_pb.CalculatorServiceClient) {
-	req := &calculator_pb.CalculatorRequest{
-		NumberOne: 3,
-		NumberTwo: 2,
+// func doUnary(c calculator_pb.CalculatorServiceClient) {
+// 	req := &calculator_pb.CalculatorRequest{
+// 		NumberOne: 3,
+// 		NumberTwo: 2,
+// 	}
+// 	res, err := c.Sum(context.Background(), req)
+// 	if err != nil {
+// 		log.Fatalf("Error when calling RPC:%v", err)
+// 	}
+// 	fmt.Printf("Result:%v", res.Result)
+// }
+
+func doServerStreaming(c calculator_pb.CalculatorServiceClient){
+	req := &calculator_pb.PrimeNumberDecompositionRequest{
+		Input:30,
 	}
-	res, err := c.Sum(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Error when calling RPC:%v", err)
+	resStream,err:=c.PrimeNumberDecomposition(context.Background(),req)
+	if err!=nil{
+		log.Fatalf("Error:%v",err)
 	}
-	fmt.Printf("Result:%v", res.Result)
+	for {
+		res, err := resStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while read streaming:%v", err)
+		}
+		// log.Printf("Res from GreetManyTimes Message :%v", res)
+		fmt.Printf("RESULT:%v\n",res.GetResult())
+	}
 }
